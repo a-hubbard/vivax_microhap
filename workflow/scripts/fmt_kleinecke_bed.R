fc714392ff74535fe5e87fbbdc4c41a60561c393
Alfred Hubbard <ahubba19@jh.edu>
2026-02-01 11:59:14 -0500

    Add script to format BED file for Kleinecke panel

---
+++ workflow/scripts/fmt_kleinecke_bed.R
@@ -0,0 +1,56 @@
+# Reformat Kleinecke et al. (2025) target spreadsheet into BED
+
+# Load required libraries
+# These libraries are referenced without the :: construction, and so 
+# are loaded second to avoid masking
+library(dplyr)
+library(magrittr)
+library(optparse)
+library(readr)
+library(readxl)
+library(stringr)
+
+# Parse arguments ------------------------------------------------------
+opts <- list(
+  make_option("--target_info", help = "Excel file containing target info"), 
+  make_option(
+    "--chrom_key", 
+    help = str_c(
+      "TSV file containing key for matching chromosome names to designations ", 
+      "used in reference genome"
+    )
+  ), 
+  make_option("--panel_bed", help = "Path for BED file describing panel")
+)
+arg <- parse_args(OptionParser(option_list = opts))
+if (interactive()) {
+  arg <- list(
+    target_info = "../../resources/41467_2025_62357_MOESM4_ESM.xlsx"
+  )
+}
+
+# Read in data ---------------------------------------------------------
+target_info <- read_excel(arg$target_info) %>%
+  select(Chromosome, Start, End, Amplicon_name, Final_set) %>%
+  filter(Final_set == 1) %>%
+  select(-Final_set)
+
+# Read and join chromosome name key ------------------------------------
+chrom_key <- read_tsv(
+    arg$chrom_key, 
+    col_names = c("chrom_name", "chrom_assembly_name"), 
+    col_types = cols(.default = col_character()), 
+    progress = FALSE
+  )
+target_info <- target_info %>%
+  left_join(chrom_key, by = c("Chromosome" = "chrom_name")) %>%
+  select(-Chromosome) %>%
+  relocate(chrom_assembly_name, .before = 1)
+
+# Add BED required columns and write to disk ---------------------------
+target_info %>%
+  # The positive strand is recorded for all targets, because the input 
+  # spreadsheet does not include strand information and the paneljudge 
+  # analyses will be unaffected by strand designation
+  mutate(score = ".", strand = "+") %>%
+  write_tsv(arg$panel_bed, col_names = FALSE)
