8217ee26f5398eb3edfc18b48b173f20f17a6a31
Alfred Hubbard <ahubba19@jh.edu>
2026-02-16 15:25:03 -0500

    Summarize sample-level read counts for publication figure

--- workflow/scripts/readcount_heatmap.R
+++ workflow/scripts/readcount_heatmap.R
@@ -16,25 +16,45 @@ opts <- list(
 arg <- parse_args(OptionParser(option_list = opts))
 if (interactive()) {
   arg$read_counts <- "../../results/AmpSeq/serialdil2/rl_read_counts.csv"
+  arg$shared_functions <- "../shared_functions.R"
+  arg$out_base <- "../../results/figs/suppfig1"
 }
 
 # Access shared functions
 source(arg$shared_functions)
 
 # Read in data ---------------------------------------------------------
-read_counts <- read_csv(
+rep_read_counts <- read_csv(
   arg$read_counts, 
   col_types = cols(
     .default = col_character(), 
+    parasitemia = col_integer(), 
     n_read = col_double()
   ), 
   progress = FALSE
 )
 
+# Summarize for each sample --------------------------------------------
+sample_read_counts <- rep_read_counts %>%
+  # Keep protocol "v2" over "v1" when both present
+  filter(parasitemia != 100 | protocol == "v2") %>%
+  group_by(sample_id, specimen_id, parasitemia, locus) %>%
+  summarize(n_read = mean(n_read), .groups = "drop")
+
 # Plot and save --------------------------------------------------------
-fig <- read_count_heatmap(read_counts)
+fig <- sample_read_counts %>%
+  select(-sample_id) %>%
+  mutate(specimen_id = str_remove(specimen_id, "pv")) %>%
+  mutate(
+    sample_id = str_c("Specimen: ", specimen_id, ", Parasitemia: ", parasitemia)
+  ) %>%
+  arrange(parasitemia, desc(specimen_id)) %>%
+  mutate(
+    sample_id = factor(sample_id, levels = unique(sample_id), ordered = TRUE)
+  ) %>%
+  read_count_heatmap()
 w <- 15
-h <- 11
+h <- 7
 ggsave(
   str_c(arg$out_base, ".pdf"), 
   plot = fig, 
