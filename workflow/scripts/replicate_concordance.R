dcb2882d783cac34e3b335b364e86a90ed493d50
Alfred Hubbard <ahubba19@jh.edu>
2026-02-26 15:49:49 -0500

    Change replicate concordance to one calculation per parasitemia

--- workflow/scripts/replicate_concordance.R
+++ workflow/scripts/replicate_concordance.R
@@ -25,26 +25,6 @@ if (interactive()) {
   arg$out_base <- "../../results/figs/suppfig3"
 }
 
-# Compute the concordance of each pair of replicates
-compute_rep_ccc <- function(rep_a_data, rep_b_data, incl_missing = FALSE) {
-  rep_a_data <- rep_a_data %>%
-    rename(rep_a_wsaf = wsaf)
-  rep_b_data <- rep_b_data %>%
-    rename(rep_b_wsaf = wsaf)
-  if (incl_missing) {
-    full_join(rep_a_data, rep_b_data, by = c("locus", "cigar")) %>%
-      replace_na(list(rep_a_wsaf = 0, rep_b_wsaf = 0)) %$%
-      DescTools::CCC(rep_a_wsaf, rep_b_wsaf) %$%
-      rho.c %$%
-      est
-  } else {
-    inner_join(rep_a_data, rep_b_data, by = c("locus", "cigar")) %$%
-      DescTools::CCC(rep_a_wsaf, rep_b_wsaf) %$%
-      rho.c %$%
-      est
-  }
-}
-
 # Read in data ---------------------------------------------------------
 allele_table <- read_tsv(
   arg$allele_table, 
@@ -66,36 +46,50 @@ rep_nums <- allele_table %>%
 allele_table <- allele_table %>%
   left_join(rep_nums, by = c("specimen_id", "parasitemia", "rep_id"))
 
-# Build data frame with all combinations of replicates -----------------
-rep_data <- allele_table %>%
-  select(-rep_id, -n_read) %>%
-  nest(allele_data = c(locus, cigar, wsaf))
-rep_combos <- expand.grid(rep_a = 1:3, rep_b = 1:3) %>%
-  as_tibble() %>%
-  filter(rep_a != rep_b)
-rep_data_combos <- rep_data %>%
-  rename(rep_a = rep_num) %>%
-  rename(rep_a_data = allele_data) %>%
-  left_join(rep_combos, by = "rep_a", relationship = "many-to-many") %>%
-  left_join(
-    rep_data, 
-    by = c("specimen_id", "parasitemia", "rep_b" = "rep_num")
+# Create combinations and compute CCC with no missing data -------------
+rep_data_long <- allele_table %>%
+  select(specimen_id, parasitemia, rep_num, locus, cigar, wsaf)
+rep_ccc_nomiss <- rep_data_long %>%
+  # Join with itself to add all combinations of replicate pairs
+  inner_join(
+    rep_data_long,
+    by = c("specimen_id", "parasitemia", "locus", "cigar"),
+    suffix = c("_a", "_b"),
+    relationship = "many-to-many"
   ) %>%
-  rename(rep_b_data = allele_data)
-
-# Compute CCC with and without FPs and FNs -----------------------------
-rep_ccc_nomiss <- rep_data_combos %>%
-  mutate(ccc = map2_dbl(rep_a_data, rep_b_data, compute_rep_ccc)) %>%
+  # Only keep unique pairs (avoid duplicates and self-comparisons)
+  filter(rep_num_a < rep_num_b) %>%
+  group_by(parasitemia) %>%
+  summarise(
+    ccc = list(DescTools::CCC(wsaf_a, wsaf_b)$rho.c),
+    .groups = "drop"
+  ) %>%
+  unnest(ccc) %>%
   mutate(missing_cond = "No FPs and FNs")
-rep_ccc_inclmiss <- rep_data_combos %>%
-  mutate(
-    ccc = map2_dbl(
-      rep_a_data, 
-      rep_b_data, 
-      compute_rep_ccc, 
-      incl_missing = TRUE
-    )
+
+# Create combinations and compute CCC, including missing data ----------
+rep_data_long_inclmiss <- rep_data_long %>%
+  complete(
+    nesting(specimen_id, parasitemia, locus, cigar), 
+    rep_num, 
+    fill = list(wsaf = 0)
+  )
+rep_ccc_inclmiss <- rep_data_long_inclmiss %>%
+  # Join with itself to add all combinations of replicate pairs
+  inner_join(
+    rep_data_long_inclmiss,
+    by = c("specimen_id", "parasitemia", "locus", "cigar"),
+    suffix = c("_a", "_b"),
+    relationship = "many-to-many"
   ) %>%
+  # Only keep unique pairs (avoid duplicates and self-comparisons)
+  filter(rep_num_a < rep_num_b) %>%
+  group_by(parasitemia) %>%
+  summarise(
+    ccc = list(DescTools::CCC(wsaf_a, wsaf_b)$rho.c),
+    .groups = "drop"
+  ) %>%
+  unnest(ccc) %>%
   mutate(missing_cond = "Incl. FPs and FNs")
 rep_ccc <- bind_rows(rep_ccc_nomiss, rep_ccc_inclmiss)
 
@@ -109,8 +103,13 @@ fig <- rep_ccc %>%
       ordered = TRUE
     )
   ) %>%
-  ggplot(mapping = aes(x = parasitemia, y = ccc)) +
-  geom_boxplot() +
+  ggplot(mapping = aes(x = parasitemia, y = est)) +
+  geom_point(size = 3) +
+  geom_errorbar(
+    mapping = aes(ymin = lwr.ci, ymax = upr.ci), 
+    width = 0, 
+    linewidth = 1
+  ) +
   facet_wrap(vars(missing_cond)) +
   labs(
     x = expression("Parasite Density (parasites" ~ "/" ~ mu ~ "L)"), 
